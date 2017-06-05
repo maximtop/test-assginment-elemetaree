e811aa419db704d955924b44a7b9ce48604e592f
Maxim Topciu <maximtop@gmail.com>
2017-06-05 23:54:47 +0300

    Adds posibility to save fetched weather history to db

--- commands/WeatherController.php
+++ commands/WeatherController.php
@@ -2,13 +2,15 @@
 
 namespace app\commands;
 
+use app\models\Weather;
 use yii\console\Controller;
 use app\components\WeatherApi;
 use Carbon\Carbon;
+use yii\helpers\Json;
+use \Yii;
 
 class WeatherController extends Controller
 {
-    private $apiDaysLimit = 30;
     public function actionIndex($start_date = null, $end_date = null)
     {
         $end_date = is_null($end_date) ? Carbon::now() : Carbon::parse($end_date);
@@ -16,12 +18,28 @@ class WeatherController extends Controller
 
         echo $end_date . "\n";
         echo $start_date . "\n";
+        $weather_history_arr = [];
 
-        while($start_date->diffInDays($end_date, false) > 0) {
+        while ($start_date->diffInDays($end_date, false) > 0) {
             $start_limit = $start_date->copy();
             $start_date = $start_date->addMonth();
             $end_limit = $start_limit->diffInMonths($end_date, false) > 0 ? $start_date : $end_date;
-            echo $start_limit . " - " . $end_limit . "\n";
+            $weather = new WeatherApi();
+            $weather_history = $weather->getWeatherHistory($start_limit->toDateString(), $end_limit->toDateString());
+//            file_put_contents('weather_array.php', var_export($weather_history, TRUE));
+
+            foreach ($weather_history['data']['weather'] as $day) {
+                array_push($weather_history_arr, [$day['date'], $day['maxtempC'], $day['mintempC']]);
+            };
+
         }
+        print_r($weather_history_arr);
+        Yii::$app->db
+            ->createCommand()
+            ->batchInsert('weather',
+                ['date', 'maxtemp', 'mintemp'],
+                $weather_history_arr)
+            ->execute();
+        echo 'done!';
     }
 }
--- components/WeatherApi.php
+++ components/WeatherApi.php
@@ -2,6 +2,7 @@
 
 namespace app\components;
 
+use yii\helpers\Json;
 use yii\httpclient\Client;
 
 
@@ -9,13 +10,11 @@ class WeatherApi
 {
     public $city = 'Moscow';
     public $base_url = 'http://api.worldweatheronline.com/premium/v1/past-weather.ashx';
-    public $start_date = '2016-01-01';
-    public $end_date = '2016-02-01';
-    public $time_period = '1';
+    public $time_period = '24';
     private $api_key = 'bff763fcb9304d4b992152328170406';
     public $format = 'json';
 
-    public function getWeatherHistory()
+    public function getWeatherHistory($start_date, $end_date)
     {
         $client = new Client();
         $response = $client->createRequest()
@@ -25,11 +24,12 @@ class WeatherApi
                 'key' => $this->api_key,
                 'q' => $this->city,
                 'format' => $this->format,
-                'date' => $this->start_date,
-                'enddate' => $this->end_date,
+                'date' => $start_date,
+                'enddate' => $end_date,
                 'tp' => $this->time_period
             ])
             ->send();
-        return $response;
+//        file_put_contents('data.json', $response->content);
+        return Json::decode($response->content);
     }
 }
